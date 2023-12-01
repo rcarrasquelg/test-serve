78cef89f1b6659d75f85261ebaeaab714f1ee89a
Roberto Carrasquel <rcarrasquel@makingsense.com>
2023-11-30 21:03:45 -0400

    changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Script from "next/script";
-const GTM_ID = "GTM-NR49565N";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,15 +22,18 @@ export default function RootLayout({
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer', ${GTM_ID})`}
+})(window,document,'script','dataLayer', 'GTM-NR49565N')`}
       </Script>
-      <body className={inter.className}>{children}</body>
-      <noscript
-        dangerouslySetInnerHTML={{
-          __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_ID}"
-        height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
-        }}
-      />
+
+      <body className={inter.className}>
+        {children}
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NR49565N"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          }}
+        />
+      </body>
     </html>
   );
 }
